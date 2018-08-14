b539185e7718205fd3140ab09a33a69eb99a0ccc
Chirag Shinde <chirag.shinde@routemobile.com>
2018-08-14 12:10:24 +0530

    used assert instead of if.

--- set1/challenge1.py
+++ set1/challenge1.py
@@ -3,5 +3,5 @@ import base64
 
 hex_str='49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d'
 final_answer='SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t'
-answer = base64.b64encode(codecs.decode(hex_str,'hex')).decode() #Always operate on raw bytes, never on encoded strings. Only use hex and base64 for pretty-printing.
-print(answer == final_answer)
+answer = base64.b64encode(codecs.decode(hex_str,'hex')).decode('ascii')
+assert(answer == final_answer)
--- set1/challenge2.py
+++ set1/challenge2.py
@@ -1,10 +1,14 @@
 import codecs
 import binascii
-hex='1c0111001f010100061a024b53535009181c'
+hex_str='1c0111001f010100061a024b53535009181c'
 xor_against='686974207468652062756c6c277320657965'
 final_answer = '746865206b696420646f6e277420706c6179'
-hex_decoded = codecs.decode(hex,'hex').decode()#.decode() Always operate on raw bytes, ne
-xor_against_decoded = codecs.decode(xor_against,'hex').decode()
-l = [ord(a) ^ ord(b) for a,b in zip(hex_decoded,xor_against_decoded)]
-answer = binascii.hexlify(bytearray(l)).decode()
-print(answer == final_answer)
+
+def fixed_xor(hex_str, xor_against):
+    hex_decoded = codecs.decode(hex_str,'hex')#.decode() Always operate on raw bytes, ne
+    xor_against_decoded = codecs.decode(xor_against,'hex')
+    l = [a ^ b for a,b in zip(hex_decoded,xor_against_decoded)]
+    answer = binascii.hexlify(bytearray(l)).decode('ascii')
+    return answer
+
+assert(fixed_xor(hex_str,xor_against) == final_answer)
